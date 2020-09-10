c06f3e9bcdac1749c778b3e60072dcfc5303edf6
Mauro-oss <mauroikv00@gmail.com>
2020-09-10 11:22:11 -0300

    Se modifico el index.ts y el server.ts

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,22 @@
-import express from "express";
+import express, { Application } from "express";
 
-class Server {
+export class Server {
+
+    app:Application;
 
     constructor()
     {
+        //hace uso de express//
+      this.app = express();
+      //se ejecuta el metodo listen//
+      this.listen();
+    }
 
+     //le da arranque al servidor//
+    listen()
+    {
+         this.app.listen(3000);
+         console.log('Servidor corriendo en el puerto 3000');
     }
 
 }
