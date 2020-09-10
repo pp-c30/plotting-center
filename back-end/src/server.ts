b131a53f76556c4a96e1edae93c1d94e01ca7b2f
Mauro-oss <mauroikv00@gmail.com>
2020-09-10 13:59:55 -0300

    se modifico server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -8,14 +8,24 @@ export class Server {
     {
         //hace uso de express//
       this.app = express();
+      //se ejecuta el metodo de configuracion//
+      this.configuracion();
       //se ejecuta el metodo listen//
       this.listen();
     }
 
+    configuracion()
+    {
+        //se seteo el puerto//
+        this.app.set('port', process.env.port || 3000)
+    }
+
+    
+
      //le da arranque al servidor//
     listen()
     {
-         this.app.listen(3000);
+         this.app.listen(this.app.get('port'));
          console.log('Servidor corriendo en el puerto 3000');
     }
 
