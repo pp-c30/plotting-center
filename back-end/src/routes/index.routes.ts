914ea831f8b99dc3b02f2be7d66d8cfde60c95d7
brunoleivaferro <brunoleivaferro@gmail.com>
2020-09-10 21:10:34 -0300

    index routes

--- back-end/src/routes/index.routes.ts
+++ back-end/src/routes/index.routes.ts
@@ -1,7 +1,10 @@
-import {Router} from "express";
+import { Router } from "express";
+
+//importamos la funcion "mensaje" desde index.controllers para que se puedan comunicar 
+import { mensaje } from "../controllers/index.controllers";
 
 let enrutadorIndex = Router();
-
-enrutadorIndex.route('/').get();
+//enrutador crea la ruta '/' a traves del metedo get y esta va a mostar lo q tenga la funcion "mensaje"
+enrutadorIndex.route('/').get(mensaje);
 
 export default enrutadorIndex;
