c93e0f83a0caaeb46877cce75c762762fa7c5410
Mauro-oss <mauroikv00@gmail.com>
2020-09-26 11:39:08 -0300

    profesion route creado

---
+++ back-end/src/routes/profesion.route.ts
@@ -0,0 +1,14 @@
+//importamos un enrutador de express
+import { Router } from "express";
+//importo profesion controller
+import { ProfesionController } from "../controllers/profesion.controller";
+
+//creamos una variable que se traiga todas las func de router
+const enrutadorProfesion = Router();
+
+//creamos una instancia para acceder a los metodos de la clase
+let profesionController = new ProfesionController();
+
+//creacion de las rutas
+
+enrutadorProfesion('/profesion').get(profesionController,listaProfesion);
