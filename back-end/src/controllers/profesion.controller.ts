ed8b3b32e22066b5f7c92e209937557d0a08a51e
Mauro-oss <mauroikv00@gmail.com>
2020-09-26 11:38:42 -0300

    profesion controller creado

---
+++ back-end/src/controllers/profesion.controller.ts
@@ -0,0 +1,11 @@
+//importo request y response
+import { Request,Response } from "express";
+
+
+export class ProfesionController
+{
+   public listaProfesion(req:Request,res:Response)
+   {
+      
+   }
+}
