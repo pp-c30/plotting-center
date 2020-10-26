97185086b326d89cd10157cd6a9c521f265d0a8f
brunoleivaferro <brunoleivaferro@gmail.com>
2020-10-26 02:47:11 -0300

    termino el cliente.routes

--- back-end/src/routes/cliente.routes.ts
+++ back-end/src/routes/cliente.routes.ts
@@ -12,7 +12,19 @@ let  clienteController = new Clientecontroller();
 const enrutadorCliente = Router();
 
 //creo la ruta "/cliente" con una petecion "get" que permitira listar a los clientes  
-enrutadorCliente.route('/cliente').get();
+enrutadorCliente.route('/cliente').get(clienteController.listaCliente);
  
 //se crea la ruta con una peticion "post" que permitira guardar los datos de los clientes en la BD
-enrutadorCliente.route('/cliente').post();
+enrutadorCliente.route('/cliente').post(clienteController.guardarCliente);
+
+//se crea la ruta con una peticion "delete" que permitira eliminar los datos de algun cliente que sea seleccionado
+enrutadorCliente.route('/cliente/:id').delete(clienteController.eliminarCliente);
+
+//ruta con una peticion "put" que permitira actulizar los datos de los cliente
+enrutadorCliente.route('/cliente/:id').put(clienteController.actulizarCliente);
+
+//ruta con una peticion "get" que permitira buscar un cliente en particular, no como le primer metodo get que me trae todos los clientes
+enrutadorCliente.route('/cliente/:id').get(clienteController.obtenerUnCliente);
+
+//expotamos el enrutador para que  pueda ser usado 
+export default enrutadorCliente;
