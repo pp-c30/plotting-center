5c6a97829e304a33a32b326eeb0ef6474ba22f5c
Mauro-oss <mauroikv00@gmail.com>
2020-11-03 23:26:57 -0300

    se modifico app modules

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GramajeComponent } from './componentes/gramaje/gramaje.component';
-<<<<<<< HEAD
 import { ProfesionComponent } from './componentes/profesion/profesion.component';
-=======
-import { ClienteComponent } from './cliente/cliente.component';
->>>>>>> 1e2d7d4b07605bf62e6101370d69ff65b7846142
+import { ClienteComponent } from './componentes/cliente/cliente.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GramajeComponent,
-<<<<<<< HEAD
-    ProfesionComponent
-=======
-    ClienteComponent
->>>>>>> 1e2d7d4b07605bf62e6101370d69ff65b7846142
-  ],
+    ProfesionComponent,
+    ClienteComponent],
+
   imports: [
     BrowserModule,
     AppRoutingModule
