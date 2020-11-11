23be2faba090af3a20ceea4ef2a9e3ac63a1e62e
Mauro-oss <mauroikv00@gmail.com>
2020-11-11 19:17:51 -0300

    se creo el modelo de impresion

---
+++ front-end/src/app/models/impresion.ts
@@ -0,0 +1,18 @@
+export interface Iimpresion 
+{
+        id_impresion?:number;
+         tipo_color: string;
+         tamanio_area:number;
+         ancho: number;
+         largo:number;
+         gramaje : number;
+         rollo : number;
+         cantidad:number;
+         correcciones: number;
+         tiempo_co:number;
+         precio_x_hora : number;
+         precio_total: number;
+         cliente :number;
+    
+    
+}
