4a802150736769d1f88a563cc9cad088d2ecd982
saezsanty <saezsanty26@gmail.com>
2020-11-20 11:59:03 -0300

    agregando elementos al gramaje service

--- front-end/src/app/services/gramaje.service.ts
+++ front-end/src/app/services/gramaje.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { HttpClient} from "@angular/common/http";
+
+import { Igramaje } from "../models/gramaje";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GramajeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    
+   }
+
+   getgramaje()
+   {
+     return this.http.get<Igramaje[]>('http://locahotst:3000/gramaje')
+   }
+
 }
