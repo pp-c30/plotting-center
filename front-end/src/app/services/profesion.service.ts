a2bd79354cd50c756088bb79c6311f3d8b9fda0d
Mauro-oss <mauroikv00@gmail.com>
2020-11-22 16:43:10 -0300

    Modificaciones al constructor del servicio

--- front-end/src/app/services/profesion.service.ts
+++ front-end/src/app/services/profesion.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+//modulo de angular para hacer peticiones http//
+import { HttpClient } from "@angular/common/http";
+//me importo el archivo de models para tener acceso a la Interfaz//
+import { IProfesion } from "../models/profesion";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesionService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  getProfesion()
+  {
+    return this.http.get<IProfesion[]>('htpp://localhost:3000/profesion');
+    
+  }
+
 }
