bb8a02a3160a159e33beea06de2ca3074fa45929
brunoleivaferro <brunoleivaferro@gmail.com>
2020-12-04 05:01:18 -0300

    se terminan de crear los servicios de las peticiones save, update y delete

--- front-end/src/app/services/cliente.service.ts
+++ front-end/src/app/services/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 //importamos un modulo de angular dentro de la carpeta coomon para utilizar el protocolo http 
 import { HttpClient } from "@angular/common/http";
 
-import { ICliente } from "../models/cliente";
+import { ICliente } from '../models/cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,28 @@ export class ClienteService {
    }
    getCliente()
    {
-     //retorna lo quer hay en la ruta
+     //hacemos una pèticon get y nos retorna lo quer hay en la ruta(o sea en la BD)
    return this.http.get<ICliente[]>('http://localhost:3000/cliente')
-   }
+  }
+
+
+  //recibe datos de unCliente (respetando las propiedades del objeto, es decir, cliente.ts) de tipo ICliente
+  saveCliente(unCliente:ICliente)
+  {
+    //a traves de una peticon http, especificamente del metodo post nos vamos a comunicar con la API 
+    this.http.post('http://localhost:3000/cliente',unCliente);
+  }
+
+  updateCliente(unCliente:ICliente)
+  {
+     let id:number = unCliente.id_cliente;
+     return this.http.put('http://localhost:3000/cliente/'+id,unCliente);
+  }
+
+  deleteCliente(id:number)
+  {
+   return this.http.delete('http://localhost:3000/cliente/'+id);
+  }
+}
 
 }
