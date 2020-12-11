a1de5f01890d52fe285e1071ee4e7d16792f2ab5
Mauro-oss <mauroikv00@gmail.com>
2020-12-11 14:43:59 -0300

    Se crearon los metodos de profesion

--- front-end/src/app/componentes/profesion/profesion.component.ts
+++ front-end/src/app/componentes/profesion/profesion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //me traigo el servicio
 import { ProfesionService } from "../../services/profesion.service";
+import { FormBuilder,FormGroup, Validators } from "@angular/forms";
+import { IProfesion } from "src/app/models/profesion";
 
 @Component({
   selector: 'app-profesion',
@@ -12,8 +14,21 @@ export class ProfesionComponent implements OnInit {
   //creo un arreglo para llenarlo con la respuestas
   lisProfesion =[];
 
+  FormProfesion: FormGroup;
+
+  buscarProfesion:any;
+
+  p:number = 1;
+
   //creo una instancia en el constructor para acceder a los metodos dentro de la clase
-  constructor(private profesionServi:ProfesionService) { }
+  constructor(private profesionServi:ProfesionService, private fb: FormBuilder) { 
+    this.FormProfesion= this.fb.group({
+
+      profesion: ['',[Validators.required,Validators.minLength(2)] ],
+      id_profesion: [null]
+
+      })
+  }
 
   ngOnInit(): void {
     //apenas inicie hacer uso de este metodo
@@ -27,5 +42,58 @@ export class ProfesionComponent implements OnInit {
       error => console.log(error)
     )
   }
+  
+  guardarProfesion(){
 
-}
+    if(this.FormProfesion.value.id_profesion)
+    {
+
+      this.profesionServi.updateProfesion(this.FormProfesion.value).subscribe(
+          respuesta =>{
+                      console.log(respuesta)
+                      this.listaProfesion();
+                      this.FormProfesion.reset();
+                      },
+
+          error => console.log(error)
+          );
+     }
+
+    else
+    {
+      this.profesionServi.saveProfesion(this.FormProfesion.value).subscribe(
+           resultado => 
+           {
+            console.log(resultado);
+            this.listaProfesion();
+            this.FormProfesion.reset();
+          }
+       );
+
+    }
+
+   }
+
+    editarProfesion(profesiones:IProfesion){
+
+      this.FormProfesion.setValue(profesiones);
+    }
+
+
+    eliminarProfesion(id:number){
+
+        if(confirm('esta seguro que quiere ejecutar esta accion?'))
+        {
+          this.profesionServi.deleteProfesion(id).subscribe(
+            respuesta  => {
+              console.log(respuesta);
+              this.listaProfesion();
+  
+            },
+            error => console.log(error)
+  
+            );
+        }
+      }
+    }
+
